c1cb49184f64da6047cfedad9529017191c7f05b
rangkim <rang0609@sookmyung.ac.kr>
2019-02-12 21:54:02 +0900

    DBHelper

---
+++ app/src/main/java/com/example/diaryapp/db/DBHelper.java
@@ -0,0 +1,25 @@
+package com.example.diaryapp.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.widget.EditText;
+
+public class DBHelper {
+    private DBManager dbmgr;
+    EditText et_name;
+    EditText et_name2;
+
+    public void dbHelper(Context context){
+        String diary_date = et_name.getText().toString();
+        String diary_content = et_name2.getText().toString();
+        try{
+            dbmgr = new DBManager(context);
+            SQLiteDatabase sdb;
+            sdb = dbmgr.getWritableDatabase();
+            sdb.execSQL("insert into diaryTB values('"+diary_date+"','"+diary_content+"');");
+            dbmgr.close();
+        } catch (
+                SQLiteException e){}
+    }
+}
