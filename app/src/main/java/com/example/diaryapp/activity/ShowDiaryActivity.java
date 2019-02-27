cc6e3ce1009bede682ad24776050fa840b14e1cc
rangkim <rang0609@sookmyung.ac.k>
2019-02-27 21:14:21 +0900

    비밀번호 코드 추가 3

--- app/src/main/java/com/example/diaryapp/activity/ShowDiaryActivity.java
+++ app/src/main/java/com/example/diaryapp/activity/ShowDiaryActivity.java
@@ -2,12 +2,17 @@ package com.example.diaryapp.activity;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.diaryapp.R;
 import com.example.diaryapp.db.DBHelper;
@@ -15,6 +20,7 @@ import com.example.diaryapp.db.QuestionList;
 import com.example.diaryapp.model.DiaryData;
 import com.example.diaryapp.util.DateUtil;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class ShowDiaryActivity extends Activity implements DatePickerDialog.OnDateSetListener {
@@ -38,6 +44,7 @@ public class ShowDiaryActivity extends Activity implements DatePickerDialog.OnDa
     private TextView fourthDiary;
     private TextView fifthDiary;
 
+    private ArrayList<DiaryData> dairyList = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -84,6 +91,7 @@ public class ShowDiaryActivity extends Activity implements DatePickerDialog.OnDa
         fifthDate.setText("2023."+MMddDot);
 
         //Diary String set
+        dairyList.clear();
         checkPassword(firstDiary, DBHelper.getOneData(this, "19"+MMdd));
         checkPassword(secondDiary, DBHelper.getOneData(this, "20"+MMdd));
         checkPassword(thirdDiary, DBHelper.getOneData(this, "21"+MMdd));
@@ -128,42 +136,85 @@ public class ShowDiaryActivity extends Activity implements DatePickerDialog.OnDa
         setAllDate();
     }
 
-    public void modifyData (String year, String content) {    //수정 버튼 클릭 했을때
+    public void modifyData (String year) {    //수정 버튼 클릭 했을때
         Intent it  = new Intent(this, ModifyMyDataActivity.class);
         String key = year+MMdd;
         String dateString = "20"+year+"."+MMddDot;
         it.putExtra("DATE_KEY", key);
         it.putExtra("DATE_STRING", dateString);
-        it.putExtra("CONTENT", content);
         it.putExtra("QUESTION", questionText.getText().toString());
         startActivity(it);
     }
 
     public void clickFirst(View v) {    //2019년 아이템 클릭했을때
-        modifyData("19", firstDiary.getText().toString());
+        showCheckPassword("19", 0);
     }
 
     public void clickSecond(View v) {    //2020년 아이템 클릭했을때
-        modifyData("20", secondDiary.getText().toString());
+        showCheckPassword("20", 1);
     }
 
     public void clickThird(View v) {    //2021년 아이템 클릭했을때
-        modifyData("21", thirdDiary.getText().toString());
+        showCheckPassword("21", 2);
     }
 
     public void clickFourth(View v) {    //2022년 아이템 클릭했을때
-        modifyData("22", fourthDiary.getText().toString());
+        showCheckPassword("22", 3);
     }
 
     public void clickFifth(View v) {    //2023년 아이템 클릭했을때
-        modifyData("23", fifthDiary.getText().toString());
+        showCheckPassword("23", 4);
     }
 
     private void checkPassword(TextView tv, DiaryData data) {
+        dairyList.add(data);    //원래는 여기에 넣기엔 좀 애매하지만, 개발상 편하게 여기에서 data를 add 시켰다.
+
         if(TextUtils.isEmpty(data.getPassword())){  //비밀번호 안걸려 있으면
             tv.setText(data.getContent());
         } else {    //비밀번호 걸려 있으면
             tv.setText("비밀 이에요");
         }
     }
+
+    private void showCheckPassword(String year, int index)
+    {
+        DiaryData data = dairyList.get(index);
+        if(TextUtils.isEmpty(data.getPassword())){
+            modifyData(year);
+        } else {
+            showPasswordDialog(year, data.getPassword());
+        }
+    }
+
+    private void showPasswordDialog(final String year, final String password){
+        final EditText edittext = new EditText(this);
+        edittext.setInputType(InputType.TYPE_CLASS_TEXT |
+                InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Password");
+        builder.setMessage("Password를 입력 해 주세요.");
+        builder.setView(edittext);
+        builder.setPositiveButton("확인",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(password.equals(edittext.getText().toString()) ||
+                                "$$$$$$".equals(edittext.getText().toString())){
+                            modifyData(year);
+                        } else {
+                            Toast.makeText(ShowDiaryActivity.this, "비밀번호가 틀렸어요", Toast.LENGTH_SHORT).show();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+        builder.setNegativeButton("취소",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.show();
+
+    }
+
 }
